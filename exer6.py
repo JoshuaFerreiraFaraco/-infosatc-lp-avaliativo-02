da81c8a11cdaf50f8110a9e0e603e892259e85a9
Joshua Ferreira Faraco <joshua.256919@alunosatc.edu.br>
2021-09-06 16:28:32 -0300

    Exer6
    Exercicio 6

---
+++ exer6.py
@@ -0,0 +1,14 @@
+lista = []
+a1 = input("Digite um valor a ser inserido na lista: ")
+a2 = input("Digite um valor a ser inserido na lista: ")
+a3 = input("Digite um valor a ser inserido na lista: ")
+a4 = input("Digite um valor a ser inserido na lista: ")
+a5 = input("Digite um valor a ser inserido na lista: ")
+
+lista.insert(0,a1)
+lista.insert(1,a2)
+lista.insert(2,a3)
+lista.append(a5)
+lista.insert(3,a4)
+
+print(lista.index("Manga"))
